e370cf9c774a4c113aa01e85465f817dfe33fe7c
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-26 15:20:22 +0500

    add controllers working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
