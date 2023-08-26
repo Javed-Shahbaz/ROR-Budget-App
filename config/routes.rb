af7b2a68628692107abf39272b8e78b262945da7
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-26 15:59:23 +0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  root to: "users#index"
+  resources :users, only: %i[index]
+
+  resources :groups, only: %i[index new create show destroy update] do
+    resources :entities, only: %i[index new create destroy update]
+  end
 end
