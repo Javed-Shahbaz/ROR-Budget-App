8376a8f2b904ceb6c39ae13034566a47d1537a49
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-08-26 15:29:33 +0500

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 } # Adjust minimum length as needed
+
+  has_many :groups, dependent: :destroy
+  has_many :entities, foreign_key: :author_id, dependent: :destroy
 end
